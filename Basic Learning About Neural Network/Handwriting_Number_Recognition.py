7042d9aa668e3928124c261f5c1f99ba285e9b1d
unknown <brandonkimwork@gmail.com>
2022-07-23 15:34:30 +0900

    Basic Neural Network

--- Basic Learning About Neural Network/Handwriting_Number_Recognition.py
+++ Basic Learning About Neural Network/Handwriting_Number_Recognition.py
@@ -1,10 +1,11 @@
+import sys
 import numpy as np
-from PIL import Image
 from mnist_helper import load_mnist
 import pickle
 
 def get_data():
-    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+    (x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
     return x_test, t_test
 
 def init_network():
@@ -34,6 +35,7 @@ def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
     
+    
     a1 = np.dot(x, W1) + b1
     z1 = sigmoid(a1)
     a2 = np.dot(z1, W2) + b2
@@ -44,16 +46,24 @@ def predict(network, x):
     return y
 
 x, t = get_data()
+
 network = init_network()        
 
-accuracy_cnt = 0 
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)
-    if p == t[i]:
-        accuracy_cnt += 1
+# Batch Size == 1
+# for i in range(len(x)):
+#      y = predict(network, x[i])
+#      p = np.argmax(y)
+#      if p == t[i]:
+#          accuracy_cnt +=1
+
+batch_size = 100
+accuracy_cnt = 0
+  
+# Batch Size == 100 
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p==t[i:i+batch_size])
     
 print("Accuracy:" + str(float(accuracy_cnt)/len(x)))
-
-
-
